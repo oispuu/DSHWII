0fe9afa7ded978174fdd01dc85891aef5ad04b88
krister.jaanhold <krister.jaanhold@transferwise.com>
2016-12-18 22:47:43 +0200

    Refactored architecture a bit

--- game.py
+++ game.py
@@ -9,25 +9,22 @@ boats = {
     "carrier": 5
 }
 
+board = [[0 for row in range(12)] for column in range(10)]
+
+for row in board:
+    row[0] = "|"
+    row[11] = "|"
+
 class Player:
     players = []
-
-    def createPlayer(self, nickName):
-        player = {}
-        player[nickName] = [[0 for row in range(12)] for column in range(10)]
-        for row in player[nickName]:
-            row[0] = "|"
-            row[11] = "|"
-        return player
 
     def __init__(self,name=None):
         self.__class__.players.append(weakref.proxy(self))
         self.name = name
 
+class Game:
 
-class Board:
-    def addBoat(self,nickName):
-        board = nickName.values()
+    def setUpBoard(self, nickName):
 
         boatType = raw_input("Select boat type: " + str(boats.keys()) + " ")
         orientation = raw_input("Select orientation (horizontal, vertical): ")
@@ -40,15 +37,15 @@ class Board:
         startY = int(raw_input("Select starting Y coordinate (1-10): "))
         while startY + boats[boatType] - 1 > 10 and orientation == "horizontal":
             print("Invalid Y coordinate, try again")
-            startY = int(raw_input("Select starting X coordinate (1-10): "))
+            startY = int(raw_input("Select starting Y coordinate (1-10): "))
 
         # check if the place is still free
         for i in range(0,boats[boatType]):
-            while orientation == "horizontal" and board[0][startX - 1][startY + i] == 1:
+            while orientation == "horizontal" and board[startX - 1][startY + i] == 1:
                 print("Place already taken, try again")
                 startX = int(raw_input("Select starting X coordinate (1-10): "))
                 startY = int(raw_input("Select starting Y coordinate (1-10): "))
-            while orientation == "vertical" and board[0][startX + i - 1][startY] == 1:
+            while orientation == "vertical" and board[startX + i - 1][startY] == 1:
                 print("Place already taken, try again")
                 startX = int(raw_input("Select starting X coordinate (1-10): "))
                 startY = int(raw_input("Select starting Y coordinate (1-10): "))
@@ -57,36 +54,32 @@ class Board:
         counter = 0
         if orientation.lower() == "horizontal":
             while counter < size:
-                board[0][startX-1][startY] = 1
+                board[startX-1][startY] = 1
                 startY += 1
                 counter += 1
         if orientation.lower() == "vertical":
             while counter < size:
-                board[0][startX-1][startY] = 1
+                board[startX-1][startY] = 1
                 startX += 1
                 counter += 1
         del boats[boatType]
-        return board
+        return {nickName: board}
 
-class Game:
     def getPlayerList(self):
         playerList = []
         for instance in Player.players:
             playerList.append(instance.name)
         return playerList
 
-    def shoot(self, player, row, column):
+    def shootAndValidate(self, player, row, column):
         enemy = raw_input("Select enemy to shoot: " + str(self.getPlayerList()))
         row = raw_input("Row: ")
         column = raw_input("Column: ")
+        player[enemy]
 
-    def setUpGame(self):
-        player = Player()
-        nickName = raw_input("Enter your nickname: ")
-        player = player.createPlayer(nickName)
-        board = Board()
-        while boats:
-            print(tabulate(board.addBoat(player)[0]))
+def setUpGame(game,player):
+    while boats:
+        print(tabulate(game.setUpBoard(player.name)[player.name]))
 
 game = Game()
 #game.setUpGame()
@@ -95,4 +88,5 @@ jaanus = Player("Jaanus")
 urmas = Player("Urmas")
 saarmas = Player("Saarmas")
 
-print(game.shoot("Jaana",3,3))
+setUpGame(game,jaanus)
+print(game.getPlayerList())
